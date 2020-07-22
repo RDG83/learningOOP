927278d16754c54896807ffd1afaca16ac525e53
Robert de Grijs <rdgrijs@hotmail.com>
2020-07-22 10:03:30 +0200

    file class to open files

---
+++ File.php
@@ -0,0 +1,17 @@
+<?php
+class File
+{
+    function displayFile($fileName)
+    {
+        try {
+            $content = file_get_contents($fileName);
+            return $content;
+        } catch (Exception $e) {
+            echo "Something went wring" . $e->getMessage();
+        }
+    }
+}
+
+$file1 = new File;
+
+echo $file1->displayFile("test.txt");
