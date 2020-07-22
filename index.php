43a8a2fab3d974e72cbb3adf919306d9b65291fc
Robert de Grijs <rdgrijs@hotmail.com>
2020-07-22 09:38:47 +0200

    add method

--- index.php
+++ index.php
@@ -10,9 +10,16 @@ class Student
         $this->firstName = $firstName;
         $this->lastName = $lastName;
     }
+
+    function hello()
+    {
+        echo "Hello, my name is $this->firstName $this->lastName.";
+    }
 }
 
 
 $student1 = new Student("Jaap", "de Vries");
 
 var_dump($student1);
+
+$student1->hello();
