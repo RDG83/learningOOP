8e676f8986536bc76511b6a071812e8eef56741e
Robert de Grijs <rdgrijs@hotmail.com>
2020-07-23 10:34:01 +0200

    add second interface to sportscar

--- interface/SportsCar.php
+++ interface/SportsCar.php
@@ -1,9 +1,21 @@
 <?php
 
 require_once "Car.php";
+require_once "CarModel.php";
 
-class SportsCar implements Car
+class SportsCar implements Car, CarModel
 {
+
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
     public function start()
     {
         echo "Starting the car. <br>";
@@ -21,6 +33,8 @@ class SportsCar implements Car
 
 $sportscar1 = new SportsCar;
 
+$sportscar1->setModel("BMW");
+echo "This car is a " . $sportscar1->getModel() . "<br>";
 $sportscar1->start();
 $sportscar1->accelerate();
 $sportscar1->break();
