01378ae827d3476f85d55b112c2888efe3f501f9
Robert de Grijs <rdgrijs@hotmail.com>
2020-07-24 11:40:27 +0200

    add minor upgrade to typehinting

--- typehinting/Car.php
+++ typehinting/Car.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 class Car
 {
+
+    private $owner = "Elon Musk";
+
     private string $name; //must be a string
     private int $model; //must be an integer
 
-    function setDetails(array $details) // only accepts arrays as input
+    function setDetails(array $details): void // only accepts arrays as input // void means it has no return value
     {
         $this->name = $details["name"];
         $this->model = $details["model"];
+    }
+
+    function getName(): string // must return a string
+    {
+        return $this->owner;
     }
 }
 
@@ -18,3 +26,4 @@ $car1 = new Car;
 $cardetails = ["name" => "Tesla", "model" => 3];
 $car1->setDetails($cardetails);
 var_dump($car1);
+echo $car1->getName();
