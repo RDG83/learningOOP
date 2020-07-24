68daef1611c7461e626ede222b9ad6ad16df688a
Robert de Grijs <rdgrijs@hotmail.com>
2020-07-24 13:38:10 +0200

    add student to dependency injection

--- dependency injection/University.php
+++ dependency injection/University.php
@@ -3,11 +3,11 @@
 class University
 {
 
-    public $UniName;
+    public $uniName;
 
     function __construct($name)
     {
-        $this->UniName = $name;
+        $this->uniName = $name;
     }
 }
 
@@ -19,27 +19,43 @@ class Course extends University
     function __construct($name, University $u)
     {
         $this->courseName = $name;
-        $this->UniName = $u->UniName;
+        $this->uniName = $u->uniName;
     }
 }
 
 class Lesson extends Course
 {
 
-    public $name;
+    public $lessonName;
 
     function __construct($name, Course $c, University $u)
     {
-        $this->name = $name;
+        $this->lessonName = $name;
         $this->courseName = $c->courseName;
-        $this->UniName = $u->UniName;
+        $this->uniName = $u->uniName;
+    }
+}
+
+class student extends Lesson
+{
+
+    public $studentName;
+
+    function __construct($name, Lesson $l, Course $c, University $u)
+    {
+        $this->studentName = $name;
+        $this->lessonName = $l->lessonName;
+        $this->courseName = $c->courseName;
+        $this->uniName = $u->uniName;
     }
 }
 
 $university1 = new University("MIT");
 $course1 = new Course("SC 101", $university1);
 $lesson1 = new Lesson("data structures", $course1, $university1);
+$student1 = new Student("John Doe", $lesson1, $course1, $university1);
 
 var_dump($university1);
 var_dump($course1);
 var_dump($lesson1);
+var_dump($student1);
