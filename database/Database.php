7a5330d4b8b3dbb2f8f4c35a678dff2f8ebc80ab
Robert de Grijs <rdgrijs@hotmail.com>
2020-07-27 17:25:34 +0200

    finished Database class

--- database/Database.php
+++ database/Database.php
@@ -16,6 +16,17 @@ class Database
 
     public function __construct()
     {
+        $dsn = 'mysql:host =' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        try {
+            $this->connection = new PDO($dsn, $this->user, $this->password, $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+        }
     }
 
     public function getError()
@@ -41,5 +52,36 @@ class Database
     {
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
+
+    public function single()
+    {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
     }
 }
